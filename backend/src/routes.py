274b00ac23bda64518d8c310c9078c298e9f8502
Rishabh Verma <me@rishabhverma.me>
2018-11-12 21:10:12 +0530

    basic api structure.

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -7,3 +7,17 @@ class StagesList(Resource):
 
     def get(self):
         return {'data': 'list of stages'}
+
+
+@api.route("/stages/<stage_id>/level")
+def LevelsList(Resource):
+
+    def get(self, stage_id):
+        return {'data': 'list of levels of given stage'}
+
+
+@api.route("/stages/<stage_id>/level/<level_id>/random_questions")
+def LevelRandomQuestionList(Resource):
+
+    def get(self, stage_id, level_id):
+        return {'data': 'list of random questions of given level'}
