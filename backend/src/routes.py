28ff048fb70853993d677d00dfe37e51526e0a82
Rishabh Verma <me@rishabhverma.me>
2018-11-13 02:24:27 +0530

    Route to get stage by ID.

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -8,14 +8,27 @@ from backend.src import api
 class StagesList(Resource):
 
     def get(self):
-
         # read the list of stages from the JSON
         stages_file = open("data/stages.json")
         stages = json.loads(stages_file.read())
         stages = stages['stages']
-
         return {'data': stages}
 
+@api.route("/stages/<stage_id>")
+class Stage(Resource):
+
+    def get(self, stage_id):
+        # read the list of stages from JSON
+        stages_file = open("data/stages.json")
+        stages = json.loads(stages_file.read())
+        stages = stages['stages']
+
+        # search for the stage & return
+        searchedStages = list(filter(lambda s: s['id'] == stage_id, stages))
+        if len(searchedStages) == 0:
+            return {'message': "Stage not found."}, 404
+        else:
+            return {'stage': searchedStages[0]}
 
 @api.route("/stages/<stage_id>/level")
 class LevelsList(Resource):
