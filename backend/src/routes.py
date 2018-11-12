c5f9801a6b7ee6f69adf6b9238466beb798bfeea
Rishabh Verma <me@rishabhverma.me>
2018-11-12 21:19:28 +0530

    list of stages route to be added.

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,3 +1,5 @@
+import json
+
 from flask_restplus import Resource
 
 from backend.src import api
@@ -6,18 +8,24 @@ from backend.src import api
 class StagesList(Resource):
 
     def get(self):
-        return {'data': 'list of stages'}
+
+        # read the list of stages from the JSON
+        stages_file = open("data/stages.json")
+        stages = json.loads(stages_file.read())
+        stages = stages['stages']
+
+        return {'data': stages}
 
 
 @api.route("/stages/<stage_id>/level")
-def LevelsList(Resource):
+class LevelsList(Resource):
 
     def get(self, stage_id):
         return {'data': 'list of levels of given stage'}
 
 
 @api.route("/stages/<stage_id>/level/<level_id>/random_questions")
-def LevelRandomQuestionList(Resource):
+class LevelRandomQuestionList(Resource):
 
     def get(self, stage_id, level_id):
         return {'data': 'list of random questions of given level'}
